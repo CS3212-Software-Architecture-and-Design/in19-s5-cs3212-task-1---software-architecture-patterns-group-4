b96ab57dcfe6c3f74063f7e0749fd9ab6075aff1
Thiniraxz <thiniragenuka@gmail.com>
2022-09-10 09:01:50 +0530

    Implement message sending

--- peer-to-peer/client.py
+++ peer-to-peer/client.py
@@ -1,22 +1,27 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 from random import randint
+import time
+
 
 class Client(DatagramProtocol):
 
     def __init__(self, port):
-        # if(host == "localhost"):
-        #     host = "127.0.0.1"
         host = "127.0.0.1"
-        self.id = host,port
-        self.address = None
+        self.id = host, port
+        self.addressToSendMsg = None
+        self.clientList = []
+        print('my address:', self.id, " Ready to send and recieve")
+        time.sleep(1)
         
-        print('my address:', self.id)
 
-
+    def send_message(self):
+        message = input(":::")
+        self.transport.write(message.encode('utf-8'), self.addressToSendMsg)
+        print(":::Message sent:::")
+        return
 
 if __name__ == '__main__':
-    port = randint(1000,5000)
+    port = randint(1000, 5000)
     reactor.listenUDP(port, Client(port))
-
     reactor.run()

1226878057c6a5f823c4a5a975be270479e5f5a9
Thiniraxz <thiniragenuka@gmail.com>
2022-09-10 10:12:44 +0530

    Implement message passing between clients

--- peer-to-peer/client.py
+++ peer-to-peer/client.py
@@ -1,3 +1,4 @@
+from nis import match
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 from random import randint
@@ -13,7 +14,42 @@ class Client(DatagramProtocol):
         self.clientList = []
         print('my address:', self.id, " Ready to send and recieve")
         time.sleep(1)
-    
+
+    def startProtocol(self):
+        self.transport.write("ready".encode('utf_8'), self.id)
+
+    def messageClients(self):
+        print("Select option 1 or 2 to send message")
+        while True:
+            print("Select option 1 or 2 to send message")
+            time.sleep(0.5)
+            print("1 - message to peer clients")
+            print("2 - message to new client")
+            try:
+                option = int(input("Enter your option: "))
+                if (option not in (1, 2)):
+                    print("Invalid input")
+                elif option == 1:
+                    if not self.clientList:
+                        print("No peers")
+                    else:
+                        print("Select a host from\n",
+                              ' '.join(self.clientList))
+                        self.addressToSendMsg = input(
+                            "host:"), int(input("port:"))
+                        self.send_message()
+
+                elif option == 2:
+                    self.addressToSendMsg = input("host:"), int(input("port:"))
+                    self.send_message()
+                    if(self.addressToSendMsg not in self.clientList):
+                        self.clientList.append(self.addressToSendMsg)
+                        time.sleep(0.5)
+
+            except:
+                print("Exception")
+                time.sleep(1)
+
     def datagramReceived(self, datagram, senderAddress):
         datagram = datagram.decode('utf-8')
         if(datagram == "ready"):
@@ -29,9 +65,10 @@ class Client(DatagramProtocol):
         message = input(":::")
         self.transport.write(message.encode('utf-8'), self.addressToSendMsg)
         print(":::Message sent:::")
+        time.sleep(1)
         return
-    
-    
+
+
 if __name__ == '__main__':
     port = randint(1000, 5000)
     reactor.listenUDP(port, Client(port))
